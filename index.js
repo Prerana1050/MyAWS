ac56b1fb5dd77c2bb06b873dd4fa6ab89cccb879
PRERANA MAITI <preranamaiti@gmail.com>
2025-04-03 11:10:08 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello MCKV')
+  res.send('Hello myself Prerana Maiti')
 })
 
 app.get('/courses', function (req, res) {
